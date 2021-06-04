dd055cea765d21347b8f07cae068af0bf1bcc355
Nomercy-ops <chauhanrikesh786@gmail.com>
2021-06-04 23:39:16 +0530

    Added userChoice method to select a letter X or O

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,11 +1,16 @@
 package com.bridgelabz;
+import java.util.Scanner;
 
 class TicTacToeGame {
 
     static char gameBoard[] = new char[10];
+	static char player = ' ';
+    static char computer = ' ';
+	
     public static void main(String[] args) {
 
         createBoard();
+		userChoice();
        
     }
 
@@ -19,4 +24,28 @@ class TicTacToeGame {
             gameBoard[i] = gameBoardElement[i];
         }
 
+    }
+	
+	/**
+     * UC-2 Method to allow the player to choose a letter X or O
+     */
+    public static void userChoice() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 1 for 'X' or 2 for 'O':");
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                player = 'X';
+                computer = 'O';
+                break;
+            case 2:
+                player = 'O';
+                computer = 'X';
+                break;
+            default:
+                System.out.println("Enter a Correct Input");
+                break;
+        }
+        System.out.println("The Player is : " + player + " and The Computer is :    " + computer);
     }
